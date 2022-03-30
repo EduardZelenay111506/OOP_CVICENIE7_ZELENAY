4d983cb0b705a4c1098a816519d5e6621c51d339
Eduard Zelenay <xzelenay@stuba.sk>
2022-03-30 16:38:27 +0200

    image

--- src/main/java/sk/stuba/fei/uim/oop/MojPanel.java
+++ src/main/java/sk/stuba/fei/uim/oop/MojPanel.java
@@ -1,15 +1,28 @@
 package sk.stuba.fei.uim.oop;
 
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+
 
 public class MojPanel extends JPanel {
     public MojPanel() {
+        BufferedImage pic = null;
+        try {
+            pic = ImageIO.read(MojPanel.class.getResourceAsStream("/logo.png"));
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+        JLabel picLabel = new JLabel(new ImageIcon(pic));
+        this.add(picLabel);
         this.setBackground(Color.YELLOW);
     }
 
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
+       // g.drawImage(); daco este
     }
 }
--- src/main/java/sk/stuba/fei/uim/oop/Okno.java
+++ src/main/java/sk/stuba/fei/uim/oop/Okno.java
@@ -2,50 +2,18 @@ package sk.stuba.fei.uim.oop;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
 
 
-public class Okno extends UniversalAdapter implements ActionListener {
+
+
+public class Okno extends JFrame {
 
     public Okno() throws HeadlessException {
-        this.setSize(400,400);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.addMouseListener(this);
-        this.setLayout(new BorderLayout());
-
-        JPanel menu = new JPanel();
-        menu.setLayout(new GridLayout(1,2));
-        menu.setBackground(Color.CYAN);
-        JLabel label = new JLabel("Tu je text vlavo");
-        JLabel label2 = new JLabel("Tu je text vpravo");
-        menu.add(label);
-        menu.add(label2);
-
-        JPanel klavesnica = new JPanel();
-        klavesnica.setLayout(new GridLayout(3,3));
-
-        for(int i=1;i<10;i++){
-            JButton b = new JButton(String.valueOf(i));
-            klavesnica.add(b);
-            b.addActionListener(this);
-            this.add(klavesnica,BorderLayout.CENTER);
-        }
-
-        this.add(menu,BorderLayout.PAGE_START);
-
+        this.add(new MojPanel());
+        this.pack();
         this.setVisible(true);
     }
 
-    @Override
-    public void mouseClicked(MouseEvent e) {
-        System.out.println(this.getComponentAt(e.getPoint()));
-    }
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-
-    }
 }
