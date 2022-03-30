be9d0280d745f14f1713cdc3cdce1b84d6b007de
Eduard Zelenay <xzelenay@stuba.sk>
2022-03-30 15:36:09 +0200

    lombok

--- src/main/java/sk/stuba/fei/uim/oop/Main.java
+++ src/main/java/sk/stuba/fei/uim/oop/Main.java
@@ -1,7 +1,11 @@
 package sk.stuba.fei.uim.oop;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-
+        Clovek c = new Clovek("meno","adresa",187,100,20,new ArrayList<Clovek>(List.of(new Clovek())));
+        System.out.println(c.toString());
     }
 }
