0366695daf74fd6901ff9cdb619046251479fe31
Eduard Zelenay <xzelenay@stuba.sk>
2022-03-30 16:22:51 +0200

    my panel

---
+++ src/main/java/sk/stuba/fei/uim/oop/MojPanel.java
@@ -0,0 +1,15 @@
+package sk.stuba.fei.uim.oop;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class MojPanel extends JPanel {
+    public MojPanel() {
+        this.setBackground(Color.YELLOW);
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        super.paintComponent(g);
+    }
+}
--- src/main/java/sk/stuba/fei/uim/oop/Okno.java
+++ src/main/java/sk/stuba/fei/uim/oop/Okno.java
@@ -6,40 +6,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 
-public class Okno extends JFrame implements ActionListener {
+public class Okno extends JFrame {
 
     public Okno() throws HeadlessException {
         super("Okno");
         this.setSize(400,400);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
-        this.setLayout(new BorderLayout());
-
-        JPanel menu = new JPanel();
-        menu.setLayout(new GridLayout(1,2));
-        menu.setBackground(Color.CYAN);
-        JLabel label = new JLabel("Tu je textvlavo");
-        JLabel label2 = new JLabel("Tu je textvprava");
-        menu.add(label);
-        menu.add(label2);
-
-        JPanel klavesnica = new JPanel();
-        klavesnica.setLayout(new GridLayout(3,3));
-
-        for(int i=1;i<10;i++){
-            JButton b = new JButton(String.valueOf(i));
-            klavesnica.add(b);
-            b.addActionListener(this);
-            this.add(klavesnica,BorderLayout.CENTER);
-        }
-
-        this.add(menu,BorderLayout.PAGE_START);
-
+        this.add(new MojPanel());
         this.setVisible(true);
-    }
-
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        System.out.println(e.getActionCommand());
     }
 }
